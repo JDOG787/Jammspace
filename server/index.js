3f4a699559d6ac13d01af01ff0e662e4cbac865b
JDOG787 <64325143+JDOG787@users.noreply.github.com>
2021-07-07 13:24:30 -0700

    fix config

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080 || process.env.PORT;
 
-app.get("hi", (req, res) => {
-    res.send("hello!");
-});
+// send hello on hi route
+app.get("/hi", (req, res) => { 
+  res.send("Hello World!");
+})
 
-// listen on port
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+// listen on port 
+app.listen(PORT, () => {   
+  console.log(`Listening on ${PORT}`);
 }
