1d5fc6bd8076eca3d074a04899599d2dcc4ed3f9
JDOG787 <64325143+JDOG787@users.noreply.github.com>
2021-07-06 21:44:10 -0700

    maybe this will fix it?

--- client/next.config.js
+++ client/next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
