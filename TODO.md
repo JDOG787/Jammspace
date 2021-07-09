6512189b9c18fb0184828a01644e73181052764e
JDOG787 <64325143+JDOG787@users.noreply.github.com>
2021-07-09 14:38:16 -0700

    add TODO.md

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO 
+
+- Set up api with grapgql and nextjs api routes ([read this](https://www.smashingmagazine.com/2020/10/graphql-server-next-javascript-api-routes/))
+- Set up webrtc 
+- Add mongodb 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const next = require('next');
 const PORT = process.env.PORT || 3000;
+const { graphqlHTTP } = require('express-graphql');
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -10,9 +11,20 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = express()
 
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
+  // use graphql express middleware
+  // server.use(
+  //   '/graphql',
+  //   graphqlHTTP({
+  //     schema: MyGraphQLSchema,
+  //     graphiql: true,
+  //   }),
+  // );
+
+  server.get('/hi', (req, res)  => res.send('hi'))
+
+  // server.all('*', (req, res) => {
+  //   return handle(req, res)
+  // })
 
   server.listen(PORT, (err) => {
     if (err) throw err
