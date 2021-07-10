8a9678aa3609ad55fe6f7c976e07f30a9364859f
JDOG787 <64325143+JDOG787@users.noreply.github.com>
2021-07-09 19:37:31 -0700

    setup graphql server

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const next = require('next');
 const PORT = process.env.PORT || 3000;
-const { graphqlHTTP } = require('express-graphql');
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -11,20 +10,9 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = express()
 
-  // use graphql express middleware
-  // server.use(
-  //   '/graphql',
-  //   graphqlHTTP({
-  //     schema: MyGraphQLSchema,
-  //     graphiql: true,
-  //   }),
-  // );
-
-  server.get('/hi', (req, res)  => res.send('hi'))
-
-  // server.all('*', (req, res) => {
-  //   return handle(req, res)
-  // })
+  server.all('*', (req, res) => {
+    return handle(req, res)
+  })
 
   server.listen(PORT, (err) => {
     if (err) throw err
