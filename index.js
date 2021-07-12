79b8870bf96b14eea0e5c3217cac6ee0bef600b5
JDOG787 <64325143+JDOG787@users.noreply.github.com>
2021-07-11 20:36:29 -0700

    try to get socketio working

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require('express')
 const next = require('next');
 const PORT = process.env.PORT || 3000;
+const http = require('http');
+const socketio = require('socket.io');
+const app = express()
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const nextApp = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
-  const server = express()
+io.on('connect', socket => {
+  socket.emit("hello", "hello world!")
+});
 
-  server.all('*', (req, res) => {
+nextApp.prepare().then(() => {
+  app.get('*', (req, res) => {
     return handle(req, res)
   })
 
   server.listen(PORT, (err) => {
     if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
   })
 })
