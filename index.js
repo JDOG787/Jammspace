5c84ccd340ff4f87008b7fd3ca17191dfcb282ac
Ray Arayilakath <72509475+RayhanADev@users.noreply.github.com>
2021-07-25 17:47:16 +0000

    fix: make server work with socketio

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
-const express = require('express')
-const next = require('next');
-const PORT = process.env.PORT || 3000;
+const express = require('express');
 const http = require('http');
+const next = require('next');
 const socketio = require('socket.io');
-const app = express()
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const { ApolloServer } = require("apollo-server-micro");
-const typeDefs = require("./utils/schemas");
-const resolvers = require("./utils/resolvers");
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const nextApp = next({ dev })
-const handle = nextApp.getRequestHandler()
+const nextHandler = nextApp.getRequestHandler()
 
 // const  apolloServer  =  new ApolloServer({  typeDefs,  resolvers  });
-
-
+// const { ApolloServer } = require("apollo-server-micro");
+// const typeDefs = require("./utils/schemas");
+// const resolvers = require("./utils/resolvers");
 
 nextApp.prepare().then(() => {
-  app.get('*', (req, res) => {
-    return handle(req, res)
-  })
+	const app = express()
+	const server = require('http').Server(app);
+	const io = new socketio.Server();
+	io.attach(server);
 
-  server.listen(PORT, (err) => {
-    if (err) throw err
-  })
+	app.get('/hello', async (req, res) => {
+		res.send('Hello World')
+	});
+
+	io.on('connection', (socket) => {
+		console.log('connection');
+		socket.emit('status', 'Hello from Socket.io');
+
+		socket.on('join-room', (roomID, userID) => {
+			console.log("hi")
+			socket.join(roomID)
+			socket.to(roomID).emit('user-connected', userID)
+		})
+
+		socket.on('disconnect', () => {
+			console.log('client disconnected');
+		});
+	});
+
+	app.all('*', (req, res) => nextHandler(req, res));
+
+	server.listen(port, () => {
+		console.log(`> Ready on Port: ${port}`);
+	});
 })
-
-io.on('connection', socket => {
-  socket.on('join-room', (roomID, userID) => {
-  console.log("hi")
-    socket.join(roomID)
-    socket.to(roomID).emit('user-connected', userID)
-  })
-});
